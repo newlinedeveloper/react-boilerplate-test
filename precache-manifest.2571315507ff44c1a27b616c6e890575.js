dcbcb15869ca145c4c4a54abb72065d5bd0c665e
newLine <60597798+newlinedeveloper@users.noreply.github.com>
2021-01-08 13:34:25 +0530

    Add build files

---
+++ precache-manifest.2571315507ff44c1a27b616c6e890575.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "afc50f63c2830a0a5658f433085796df",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad4c4a148d231008b475",
+    "url": "/static/css/2.3e575b9f.chunk.css"
+  },
+  {
+    "revision": "3630a85cf8172503938e",
+    "url": "/static/css/main.9c6cdb86.chunk.css"
+  },
+  {
+    "revision": "ad4c4a148d231008b475",
+    "url": "/static/js/2.4a6931a2.chunk.js"
+  },
+  {
+    "revision": "0f2be52aac348a57b5e1722f475cbfd0",
+    "url": "/static/js/2.4a6931a2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3630a85cf8172503938e",
+    "url": "/static/js/main.34f19e4d.chunk.js"
+  },
+  {
+    "revision": "63c2b9e2298cc185c024",
+    "url": "/static/js/runtime-main.2865f0c5.js"
+  }
+]);
